f6a2c3091e5dfeeae6408a8ba1f7b7c17b72463b
TejasParse <tejasparse1@gmail.com>
2022-06-24 00:07:38 +0530

    Heroku Deploy

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const bcrypt = require('bcrypt');
 const session = require("express-session");
 const flash = require('connect-flash');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 database.connectDatabase()
     .then(()=>{
